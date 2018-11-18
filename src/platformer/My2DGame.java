e3e7e5784e316c89d544b73d346f2f0a27ee481e
javasaurus <javasaurusdev@gmail.com>
2018-11-18 15:06:15 +0100

    Initial commit

---
+++ src/platformer/My2DGame.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 DrJavaSaurus <javasaurusdev@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package platformer;
+
+import platformer.control.system.Simple2DApplication;
+import platformer.model.sprites.AnimatedSprite;
+import platformer.model.data.AnimationData;
+import platformer.model.data.SpriteData;
+import platformer.model.sprites.StaticSprite;
+
+public class My2DGame extends Simple2DApplication {
+
+    public static void main(String[] args) {
+        My2DGame app = new My2DGame();
+        app.start();
+    }
+
+    private AnimatedSprite[] animatedSprites;
+
+    @Override
+    public void simpleInit2DApp() {
+        int movingSprites = 5000;
+        animatedSprites = new AnimatedSprite[movingSprites];
+        for (int i = 0; i < movingSprites; i++) {
+            animatedSprites[i] = new AnimatedSprite("Textures/Sprites.png");
+
+            AnimationData data = new AnimationData("Idle", 5, 5, 0, 25, 2 * i, true);
+            //can set an animation or add multiple to the animated sprite object to pick one later
+            animatedSprites[i].setAnimation(data);
+            animatedSprites[i].setLocalTranslation((float) (-0.5 + Math.random()) * 5f, (float) (-0.5 + Math.random()) * 5f);
+            rootNode.attachChild(animatedSprites[i]);
+        }
+
+        StaticSprite staticSprite = new StaticSprite("TestingSprite", "Textures/Sprites.png", new SpriteData(5, 5, 0));
+        staticSprite.setLocalTranslation(1f, 0f);
+    //    staticSprite.setRenderingLayer(1);
+        rootNode.attachChild(staticSprite);
+
+        staticSprite = new StaticSprite("TestingSprite", "Textures/Sprites.png", new SpriteData(5, 5, 15));
+        staticSprite.setLocalTranslation(-1f, 0f);
+    //    staticSprite.setRenderingLayer(-1);
+        rootNode.attachChild(staticSprite);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        for (AnimatedSprite animatedSprite : animatedSprites) {
+            animatedSprite.rotate2D(tpf * animatedSprite.getCurrentAnimation().getPlaybackSpeed());
+            float x = (float) (Math.cos(getTimer().getTimeInSeconds())) * 2f;
+            float y = (float) (Math.sin(getTimer().getTimeInSeconds())) * 2f;
+            animatedSprite.setLocalTranslation(x, y);
+        }
+
+    }
+
+}
---
+++ src/platformer/model/data/AnimationData.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2018 DrJavaSaurus <javasaurusdev@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package platformer.model.data;
+
+/**
+ * This class is a simple wrapper for animation parameters with the intent to serialize to json/xml later
+ * @author DrJavaSaurus <javasaurusdev@gmail.com>
+ */
+public class AnimationData extends SpriteData {
+
+    /**
+     * The name of the animation
+     */
+    private final String animationName;
+    /**
+     * The framelength of the animation
+     */
+    private final int animationLength;
+    /**
+     * The playback speed for the animation
+     */
+    private final int playbackSpeed;
+    /**
+     * Boolean to indicate if the animation should be looping
+     */
+    private final boolean looping;
+
+    /**
+     * 
+     * @param animationName The name of the animation
+     * @param columns The amount of columns in the spritesheet
+     * @param rows The amount of rows in the spritesheet
+     * @param spriteIndex The starting index for the sprite (corresponds to the sprite index for static sprites)
+     * @param animationLength The framelength of the animation
+     * @param playbackSpeed The playback speed for the animation
+     * @param looping Boolean to indicate if the animation should be looping
+     */
+    public AnimationData(String animationName, int columns, int rows, int spriteIndex, int animationLength, int playbackSpeed, boolean looping) {
+        super(columns,rows,spriteIndex);
+        this.animationName = animationName;
+        this.animationLength = animationLength;
+        this.playbackSpeed = playbackSpeed;
+        this.looping = looping;
+    }
+
+    public String getAnimationName() {
+        return animationName;
+    }
+
+    public int getAnimationLength() {
+        return animationLength;
+    }
+
+    public int getPlaybackSpeed() {
+        return playbackSpeed;
+    }
+
+    public boolean isLooping() {
+        return looping;
+    }
+
+}
